35c987239837f81fd1d3d52fa6f424cb49007c0f
pbrearton94 <pbrearton@gmail.com>
2023-06-09 08:02:00 -0400

    Initial setup of the new crown clothing repository.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      subtitle: "",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      subtitle: "",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      subtitle: "",
+    },
+    {
+      id: 4,
+      title: "Mens",
+      subtitle: "",
+    },
+    {
+      id: 5,
+      title: "Women",
+      subtitle: "",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
