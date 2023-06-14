950bead3b444b1636a33500dccb7010c3a75d3a2
pbrearton94 <pbrearton@gmail.com>
2023-06-14 14:26:50 -0400

    Created category preview component.

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./category-preview.styles.scss";
-const CategoryPreview = () => {
-  return <div></div>;
+import ProductCard from "../product-card/product-card.component";
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className="category-preview-container">
+      <h2>
+        <span className="title">{title.toUpperCase()}</span>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, i) => i < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -7,22 +7,19 @@ import {
 } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 import "./shop.styles.scss";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 
